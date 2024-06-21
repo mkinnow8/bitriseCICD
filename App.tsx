a818f300e9b9e1f4d6e99a8cf4d3cda6a97654b3
Manan <mkumar@innow8apps.com>
2024-06-21 18:11:37 +0530

    Changes Hello

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {SafeAreaView, StyleSheet, Text} from 'react-native';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text> Hello World! Again </Text>
+      <Text> Hello ! </Text>
     </SafeAreaView>
   );
 }
